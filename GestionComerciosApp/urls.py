09331249784c16235695ac1c84bd201ed43c5110
Cristian1335 <78290774+Cristian1335@users.noreply.github.com>
2021-02-25 23:32:35 -0300

    Creacion de localidades

--- GestionComerciosApp/urls.py
+++ GestionComerciosApp/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import home
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import localidades
 
 urlpatterns = [
     path('',home, name='home'),
+    path('Localidades',localidades, name='Localidades'),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- GestionComerciosApp/views.py
+++ GestionComerciosApp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from django.template import Context
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home.html')
+
+def localidades(request):
+
+    lugares = ['Resistencia','Avia Terai','Campo Largo','Charata','Colonia Elisa','Colonia Benitez','Colonias Unidad','Comandancia Frías','Concepción del Bermejo','Coronel Du Graty','Corzuela','El Paranacito','El Sauzalito','Fortin Belgrano','Ganacedo','Gral. José de San Martín','General Pinedo','Hermoso Campo','Isla Soto','Juan José Castelli','La Clotilde','La Escondida','La Leonesa','La Tigra','La Verde','Las Breñas','Las Garcitas','Las Hacheras','Las Palmas','Los Frentones','Machagai','Makallé','Margarita Belén','Miraflores','Misión Nueva Pompeya','Napenay','Pampa del Indio','Pampa del Infierno','Presidencia de la Plaza','Presidencia Roca','Puerto Bermejo','Puerto Las Palmas','Puerto Tirol','Quitilipi','San Bernardo','Paraje San Fernando','Santa Sylvina','Taco Pozo','Tres Isletas','Villa Ángela','Villa Berthet','Villa Río Bermejito']
+
+    return render(request, 'localidades.html', {"lugares":lugares})
