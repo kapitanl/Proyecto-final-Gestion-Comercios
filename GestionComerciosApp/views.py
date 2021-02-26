4e5bb82a5d7b261bf6dc3413d3084ae4fb3f0c91
Cristian1335 <78290774+Cristian1335@users.noreply.github.com>
2021-02-26 00:11:40 -0300

    Actualización de localidades en html

--- GestionComerciosApp/views.py
+++ GestionComerciosApp/views.py
@@ -8,6 +8,5 @@ def home(request):
 
 def localidades(request):
 
-    lugares = ['Resistencia','Avia Terai','Campo Largo','Charata','Colonia Elisa','Colonia Benitez','Colonias Unidad','Comandancia Frías','Concepción del Bermejo','Coronel Du Graty','Corzuela','El Paranacito','El Sauzalito','Fortin Belgrano','Ganacedo','Gral. José de San Martín','General Pinedo','Hermoso Campo','Isla Soto','Juan José Castelli','La Clotilde','La Escondida','La Leonesa','La Tigra','La Verde','Las Breñas','Las Garcitas','Las Hacheras','Las Palmas','Los Frentones','Machagai','Makallé','Margarita Belén','Miraflores','Misión Nueva Pompeya','Napenay','Pampa del Indio','Pampa del Infierno','Presidencia de la Plaza','Presidencia Roca','Puerto Bermejo','Puerto Las Palmas','Puerto Tirol','Quitilipi','San Bernardo','Paraje San Fernando','Santa Sylvina','Taco Pozo','Tres Isletas','Villa Ángela','Villa Berthet','Villa Río Bermejito']
 
-    return render(request, 'localidades.html', {"lugares":lugares})
+    return render(request, 'localidades.html', )
