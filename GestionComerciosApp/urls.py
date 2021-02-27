d21a04fcf64b223077964fada848acb2e28a855e
Cristian1335 <78290774+Cristian1335@users.noreply.github.com>
2021-02-27 17:07:56 -0300

    A ver ahora

--- GestionComerciosApp/urls.py
+++ GestionComerciosApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home, localidades 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('',home, name='home'),
+    path('Localidades',localidades, name='localidades')
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
